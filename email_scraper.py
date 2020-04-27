bfa178a644c32c131b44a5ba4b999c4f16d625c3
Kim-Chan Tze Kuo Wyz <wkimchan@avodaq.com>
2020-04-27 13:56:28 +0800

    Added chrome drivers and OS check.

--- email_scraper.py
+++ email_scraper.py
@@ -1,9 +1,35 @@
 from selenium import webdriver
 from openpyxl import Workbook
+from sys import platform
+import os
+import stat
 
 email_link = "https://sis.moe.gov.sg/default.aspx?search=directory"
 
-driver = webdriver.Chrome()
+
+# Check OS and set driver location accordingly
+if platform == "win32" or platform == "cygwin":
+    driver_location = "webdrivers/windows/chromedriver.exe"
+    system_os = "Windows"
+elif platform == "darwin":
+    driver_location = "webdrivers/mac/chromedriver"
+    # Set to executable by owner (This already works)
+    os.chmod(driver_location, stat.S_IEXEC)
+
+    # Set to read write executable by all (If the above permissions do not work, uncomment the line below this one)
+    # os.chmod(driver_location, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+
+    system_os = "Mac OS X"
+elif platform == "linux" or platform == "linux2":
+    driver_location = "webdrivers/linux/chromedriver"
+    # Set to executable by owner
+    os.chmod(driver_location, stat.S_IEXEC)
+
+    # Set to read write executable by all (If the above permissions do not work, uncomment the line below this one)
+    # os.chmod(driver_location, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    system_os = "Linux"
+
+driver = webdriver.Chrome(driver_location)
 driver.maximize_window()
 driver.implicitly_wait(10)
 driver.get(email_link)
